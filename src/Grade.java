33f279ce24ee966235b3ceed6d85763d179055d4
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-25 14:21:53 -0400

    Create variables for the Grade Classes

---
+++ src/Grade.java
@@ -0,0 +1,11 @@
+public class Grade {
+    private double score;
+    private String courseName;
+    private int courseId;
+
+    public Grade(double score, String courseName, int courseId){
+        this.score = score;
+        this.courseName = courseName;
+        this.courseId = courseId;
+    }
+}
