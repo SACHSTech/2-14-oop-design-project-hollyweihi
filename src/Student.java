2b8aa7cfadf78cf5e5be70fd94c17b76dc6167fb
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-25 14:13:05 -0400

    Create variable for Student class

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,11 @@
-public class Student {
+public class Student extends Person{
+    private int studentId;
+    private int absent;
+
+    public Student(String name, int studentId, int absent){
+        super(name);
+        this.studentId = studentId;
+        this.absent = absent;
+    }
 
 }
