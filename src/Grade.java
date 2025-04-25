35347e92d72d7bb97a80625bc28a78da758d9413
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-25 14:30:58 -0400

    Start converting Percentage to GPA not finish yet

--- src/Grade.java
+++ src/Grade.java
@@ -20,4 +20,22 @@ public class Grade {
     public int getCourseId(){
         return courseId;
     }
+
+    public double getGPA(){
+        double GPA = 0;
+
+        if(score >= 85 && score < 100){
+            GPA = 4.0;
+            return GPA;
+        }else if (score >= 80 && score < 85){
+            GPA = 3.7;
+            return GPA;
+        }else if(score >= 75 && score < 80){
+            GPA = 3.3;
+            return GPA;
+        }else if (score >= 70 && score < 75){
+            GPA = 3.0;
+            return GPA;
+        }
+    }
 }
