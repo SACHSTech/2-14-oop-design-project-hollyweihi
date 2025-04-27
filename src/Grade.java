b28c4708fc9d9a705ee073cb8638b4407a4a8b4c
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-27 12:45:44 -0400

    Create getLevel Grade method

--- src/Grade.java
+++ src/Grade.java
@@ -26,4 +26,44 @@ public class Grade {
         GPA = (score/100) * 4;
         return GPA;
     }
+
+    public String getLevelGrade(){
+        String level = "";
+
+        if(score >= 95){
+            level = "A+";
+
+        } else if(score >= 87 && score < 95){
+            level = "A";
+        }else if(score >= 80 && score < 87){
+            level = "A-";
+
+        }else if (score >= 77 && score < 80){
+            level = "B+";
+        }else if(score >= 73 && score < 77){
+            level = "B";
+        }else if (score >= 70 && score<73){
+            level = "B-";
+        }else if(score >=67 && score <70){
+            level = "C+";
+        }else if (score >=63 && score< 67){
+            level = "C";
+
+        }else if(score >= 60&& score <63){
+            level = "C-";
+
+        }else if (score >=57 && score < 60){
+            level = "D+";
+
+        }else if(score >= 53 && score < 57){
+            level = "D";
+        }else if (score >= 50 && score <53){
+            level = "D-";
+        }else if (score <50){
+            level = "I";
+        }else if (score >100){
+            level = "Not a possible score";
+        }
+       return level;
+    }
 }
