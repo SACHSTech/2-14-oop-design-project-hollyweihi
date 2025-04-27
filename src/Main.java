1ed235f6ce50b3d667f6def81d0440fb3bc55af8
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-27 13:14:40 -0400

    Set up genertal detail of course system

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,31 @@ public class Main {
         int absend = Integer.parseInt(keyboard.readLine());
 
         List <Grade> grades = new ArrayList();
+
+        System.out.println("Please Enter the Number of Course you have: ");
+        int numberOfCourse = Integer.parseInt(keyboard.readLine());
+        if (numberOfCourse == 1){
+            System.out.println("Enter Detail for course");
+        } else if (numberOfCourse == 2){
+            System.out.println("Please enter Detail for Course 1");
+            System.out.println("Please enter Detail for Course 2");
+        }else if(numberOfCourse == 3){
+            System.out.println("Please enter Detail for Course 1");
+            System.out.println("Please enter Detail for Course 2");
+            System.out.println("Please enter Detail for Course 3");
+        }else if(numberOfCourse == 4){
+            System.out.println("Please enter Detail for Course 1");
+            System.out.println("Please enter Detail for Course 2");
+            System.out.println("Please enter Detail for Course 3");
+            System.out.println("Please enter Detail for Course 4");
+        }else if(numberOfCourse == 5){
+            System.out.println("Please enter Detail for Course 1");
+            System.out.println("Please enter Detail for Course 2");
+            System.out.println("Please enter Detail for Course 3");
+            System.out.println("Please enter Detail for Course 4");
+            System.out.println("Please enter Detail for Course 5");
+        }else{
+            System.out.println("Not possible number of course.");
+        }
     }
 }
