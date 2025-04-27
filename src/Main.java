61cb3238a2b2d044723ab79fa56863c7ba425ca3
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-27 13:04:24 -0400

    Ask user to input their ID

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please Enter your Student/Employee ID: ");
+        int studentId = Integer.parseInt(keyboard.readLine());
+    }
+}
