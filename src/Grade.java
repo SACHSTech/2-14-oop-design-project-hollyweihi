a84dd50c86c1aec9da595660b8428c2a8f977ea8
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-27 12:36:37 -0400

    Update calculation formula for Percentage to GPA

--- src/Grade.java
+++ src/Grade.java
@@ -23,19 +23,7 @@ public class Grade {
 
     public double getGPA(){
         double GPA = 0;
-
-        if(score >= 85 && score < 100){
-            GPA = 4.0;
-            return GPA;
-        }else if (score >= 80 && score < 85){
-            GPA = 3.7;
-            return GPA;
-        }else if(score >= 75 && score < 80){
-            GPA = 3.3;
-            return GPA;
-        }else if (score >= 70 && score < 75){
-            GPA = 3.0;
-            return GPA;
-        }
+        GPA = (score/100) * 4;
+        return GPA;
     }
 }
