40558abb252728b5c1d909476ca31901c0d5dcba
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-04-28 11:18:05 -0400

    Ask for user to input their score

--- src/Main.java
+++ src/Main.java
@@ -26,13 +26,15 @@ public class Main {
             String courseCode = keyboard.readLine();
 
             double totalGrade = 0;
-            double grade = 1;
-            while(grade != 0){
-                System.out.println("Enter the grade(Enter 0 if there are no more grade): ");
-                grade = Double.parseDouble(keyboard.readLine());
-
-                totalGrade += grade;
+            double score = 1;
+            while(score != 0){
+                System.out.println("Enter the score(Enter 0 if there are no more score): ");
+                score = Double.parseDouble(keyboard.readLine());
+                grades.add(new Grade(score, courseName, courseCode));
+                totalGrade += score;
             } 
+
+
 
 
 
