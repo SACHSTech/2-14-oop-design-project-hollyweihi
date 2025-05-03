00f49084b5d49141821e9cb745ee1360e3376c15
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-02 22:32:31 -0400

    Add javadoc and fix format for Teacher.java class

--- src/Teacher.java
+++ src/Teacher.java
@@ -6,6 +6,17 @@ public class Teacher extends Person {
     private String department;
     private List<String> courseTeaching;
 
+    /**
+     * Construct a new teacher.
+     * 
+     * @param name           The name of teacher
+     * @param employeeId     The employee ID of a teacher
+     * @param department     The department of teacher
+     * @param courseTeaching The courses that a teacher have
+     * 
+     * @author Holly Wei
+     */
+
     public Teacher(String name, int employeeId, String department, List<String> courseTeaching) {
         super(name);
         this.employeeId = employeeId;
@@ -13,25 +24,45 @@ public class Teacher extends Person {
         this.courseTeaching = new ArrayList<>();
     }
 
+    /**
+     * Return the employee id.
+     * 
+     * @return employee id
+     */
     public int getEmployeeId() {
         return employeeId;
     }
 
-    public String getDepartment(){
+    /**
+     * Return the department of the teacher.
+     * 
+     * @return the department
+     */
+    public String getDepartment() {
         return department;
     }
 
-    public List<String> getCoursesTeaching(){
+    /**
+     * Return the course that a teacher teach.
+     * 
+     * @return course teaching by a teacher
+     */
+    public List<String> getCoursesTeaching() {
         return courseTeaching;
     }
 
-    public void addCourse(String courseCode){
-        if(!courseTeaching.contains(courseCode)){
+    /**
+     * Adding a course to a list of course.
+     * 
+     * @param courseCode The course code of a course.
+     */
+    public void addCourse(String courseCode) {
+        if (!courseTeaching.contains(courseCode)) {
             courseTeaching.add(courseCode);
         }
     }
 
-    public String toString(){
-        return "Teacher: " + getName() + "(" + getEmployeeId() + ") have " + getCoursesTeaching(); 
+    public String toString() {
+        return "Teacher: " + getName() + "(" + getEmployeeId() + ") have " + getCoursesTeaching();
     }
 }
