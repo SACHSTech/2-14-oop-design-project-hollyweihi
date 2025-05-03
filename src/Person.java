e7c18a99a34a67339b15d61f8d9ad20fcfc27548
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-02 22:23:11 -0400

    Add javadoc for Person.java Class

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,22 @@
 public class Person {
     private String name;
 
+    /**
+     * Construct a Grade Calculator, allowing user to calculate their average and convert them to GPA, Letter Grade, Level Grade. 
+     * @param name The name of user
+     * 
+     * @author Holly Wei
+     */
+
     public Person(String name) {
         this.name = name;
     }
 
+    /**
+     * Return user name.
+     * 
+     * @return user name
+     */
     public String getName() {
         return name;
     }
