a7d62b18e57c9e78c9f407c0594c36ae2ca08854
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-03 10:31:27 -0400

    Create new array List for teacher and stuent

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
+
+    private static List<Teacher> teachers = new ArrayList();
+    private static List<Student> students = new ArrayList();
+    
     public static void main(String[] args) throws IOException {
         
 
