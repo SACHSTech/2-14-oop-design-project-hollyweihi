c42c256816ba7c342c3b40d5770e4d7d98e3c94e
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-02 22:34:35 -0400

    Add javadoc and fix format for student.java Class

--- src/Student.java
+++ src/Student.java
@@ -5,20 +5,45 @@ public class Student extends Person {
     private int studentId;
     private List<Grade> grades;
 
+    /**
+     * Construct a new student.
+     * 
+     * @param name      The name of a student
+     * @param studentId The student ID of a student
+     * @param grades    The grade that student have
+     * 
+     * @author Holly Wei
+     */
+
     public Student(String name, int studentId, List<Grade> grades) {
         super(name);
         this.studentId = studentId;
         this.grades = new ArrayList();
     }
 
+    /**
+     * Return student ID.
+     * 
+     * @return student ID
+     */
     public int getStudentID() {
         return studentId;
     }
 
-    public List<Grade> getGrades(){
+    /**
+     * Return student's grade.
+     * 
+     * @return the grades
+     */
+    public List<Grade> getGrades() {
         return grades;
     }
 
+    /**
+     * Calculate the student average.
+     * 
+     * @return average
+     */
     public double getAverage() {
         double totalGrade = 0.00;
 
