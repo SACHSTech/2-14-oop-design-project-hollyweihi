e54a81226245e99f57fd27f6ee04803b34c6fb81
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-03 10:32:11 -0400

    Add two teacher into the list

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-
     private static List<Teacher> teachers = new ArrayList();
     private static List<Student> students = new ArrayList();
-    
-    public static void main(String[] args) throws IOException {
-        
 
+    public static void main(String[] args) throws IOException {
         Teacher mathTeacher = new Teacher("Mr.Smith", 100001, "Math");
         Teacher scienceTeacher = new Teacher("Dr. Johnson", 100002, "Science");
+        teachers.add(mathTeacher);
+        teachers.add(scienceTeacher);
 
         Student student1 = new Student("Holly", 333333);
         Student student2 = new Student("Bob", 200002);
